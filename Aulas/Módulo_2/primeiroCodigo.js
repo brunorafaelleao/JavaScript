49dd450215f540b63af365eaee2b2e31d66e2a12
Bruno Leão <bruno.melosh@gmail.com>
2024-07-16 23:01:11 -0400

    aula sobre variaveis

--- Aulas/Módulo_2/primeiroCodigo.js
+++ Aulas/Módulo_2/primeiroCodigo.js
@@ -1,1 +1,11 @@
-console.log("Eu escrevi meu primeiro código js");
+let mensagem = "Meu nome é Bruno Rafael e tenho 34 anos";
+console.log(mensagem);
+console.log(mensagem);
+
+console.log(mensagem);
+
+console.log(mensagem);
+
+mensagem = "Karoline Ribeiro";
+
+console.log(mensagem);
