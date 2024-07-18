90571483b7786e2aa46c8d5b30dc3b9f207199a5
Bruno Leão <bruno.melosh@gmail.com>
2024-07-18 13:14:52 -0400

    aula sobre operadores e tipos de dados

---
+++ Aulas/Módulo_2/operadores.js
@@ -0,0 +1,14 @@
+let resultado = 3;
+resultado--;
+
+//console.log(resultado);
+
+//+ adição, - subtração, * multiplicação, / divisão, % módulo
+
+const texto1 = "Olá,";
+
+const texto2 = "mundo!";
+
+const texto3 = texto1 + " " + texto2;
+
+console.log(texto3);
