efa026c7f37e1acb1ee3c858052d674719e2206b
Bruno Leão <bruno.melosh@gmail.com>
2024-07-18 14:11:23 -0400

    exercicio 1 respondido

---
+++ Exercícios/ex01/exercicio01.js
@@ -0,0 +1,18 @@
+const valor1 = 2148;
+let valor2;
+
+console.log(`O valor da primeira variável é ${valor1}`);
+
+valor2 = valor1 ** 0;
+
+console.log(
+  `Minha segunda variável vale ${valor2} e o valor da soma das minhas duas variáveis é ${
+    valor1 + valor2
+  }`
+);
+
+let valor3 = valor2 ** 2;
+
+console.log(valor3);
+
+console.log(valor3 / 3);
