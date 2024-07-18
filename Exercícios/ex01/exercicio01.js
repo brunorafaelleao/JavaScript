e6c93593e36c22f284efff3dd1eb477b7595d3b6
Bruno Leão <bruno.melosh@gmail.com>
2024-07-18 15:14:49 -0400

    resolução exercicio 2

--- Exercícios/ex01/exercicio01.js
+++ Exercícios/ex01/exercicio01.js
@@ -3,7 +3,7 @@ let valor2;
 
 console.log(`O valor da primeira variável é ${valor1}`);
 
-valor2 = valor1 ** 0;
+valor2 = 6;
 
 console.log(
   `Minha segunda variável vale ${valor2} e o valor da soma das minhas duas variáveis é ${
@@ -11,7 +11,7 @@ console.log(
   }`
 );
 
-let valor3 = valor2 ** 2;
+const valor3 = valor2 ** 2;
 
 console.log(valor3);
 
