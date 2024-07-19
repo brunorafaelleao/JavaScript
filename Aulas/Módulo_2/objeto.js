158581622e929df710c8cfcb493940d231aaa017
Bruno Leão <bruno.melosh@gmail.com>
2024-07-19 09:06:03 -0400

    Aula sobre Object( objeto)

---
+++ Aulas/Módulo_2/objeto.js
@@ -0,0 +1,13 @@
+const documentoIdentidade = {
+  nome: "Bruno",
+  sobrenome: "Leão",
+  cpf: "944.965.012-87",
+  empresa: "SEMSA",
+  idade: 34,
+};
+
+console.log(documentoIdentidade);
+
+console.log(documentoIdentidade.sobrenome);
+
+console.log(documentoIdentidade["nome"]);
