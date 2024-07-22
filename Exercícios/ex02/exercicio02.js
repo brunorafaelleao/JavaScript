4ee5e818c043bab3421d2a14d13f0745099d6441
Bruno Leão <bruno.melosh@gmail.com>
2024-07-22 17:30:26 -0400

    Resolução do exercício 2

---
+++ Exercícios/ex02/exercicio02.js
@@ -0,0 +1,32 @@
+console.log("------Resultado do exercício 01------");
+const lista = [1, 2, 3, 4, 5];
+
+console.log(lista[0]);
+
+console.log(`O valor do elemnto 3 da minha lista é: ${lista[2]}`);
+
+console.log("\n\n------Resultado do exercício 02------");
+
+let pokemon = {
+  nome: "Carapanã",
+  estamina: "2",
+  defesa: "200",
+  ataque: "300",
+  habilidade: "Voo",
+};
+
+console.log(`Meu objeto pokemon possui defesa ${pokemon.defesa}`);
+
+console.log("\n\n------Resultado do exercício 03------");
+const val1 = 30;
+const val2 = 10;
+
+console.log(`A soma de ${val1} e ${val2} é: ${val1 + val2}`);
+console.log(`A subtração de ${val1} e ${val2} é: ${val1 - val2}`);
+console.log(`A multiplicação de ${val1} e ${val2} é: ${val1 * val2}`);
+console.log(`A divisão de ${val1} e ${val2} é: ${val1 / val2}`);
+
+console.log("\n\n------Resultado do exercício 04------");
+console.log(val1 > val2);
+console.log(val1 < val2);
+console.log(val1 === val2);
