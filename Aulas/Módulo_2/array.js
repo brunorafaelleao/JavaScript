ae2bf46dd940ba3c6010e2e89f9f68972b00e598
Bruno Leão <bruno.melosh@gmail.com>
2024-07-22 09:15:29 -0400

    dicas de uso de array

--- Aulas/Módulo_2/array.js
+++ Aulas/Módulo_2/array.js
@@ -1,1 +1,11 @@
-const investimentosMensal = [];
+const investimentoMensal = [1000, 2000, 3000, 2000, 1500, 1200, 1300];
+
+console.log(`O valor investido no mês de janeiro é: ${investimentoMensal[0]}`);
+console.log(
+  `O valor investido no mês de fevereiro é: ${investimentoMensal[01]}`
+);
+console.log(`O valor investido no mês de março é: ${investimentoMensal[02]}`);
+console.log(`O valor investido no mês de abril é: ${investimentoMensal[03]}`);
+console.log(`O valor investido no mês de maio é: ${investimentoMensal[04]}`);
+
+//console.log(investimentoMensal);
