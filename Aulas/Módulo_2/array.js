1d9202aac45f54bb05c3c2292cc65782f5a2e77e
Bruno Leão <bruno.melosh@gmail.com>
2024-07-22 09:36:51 -0400

    inserir dados em um object ou array

--- Aulas/Módulo_2/array.js
+++ Aulas/Módulo_2/array.js
@@ -2,10 +2,10 @@ const investimentoMensal = [1000, 2000, 3000, 2000, 1500, 1200, 1300];
 
 console.log(`O valor investido no mês de janeiro é: ${investimentoMensal[0]}`);
 console.log(
-  `O valor investido no mês de fevereiro é: ${investimentoMensal[01]}`
+  `O valor investido no mês de fevereiro é: ${investimentoMensal[1]}`
 );
-console.log(`O valor investido no mês de março é: ${investimentoMensal[02]}`);
-console.log(`O valor investido no mês de abril é: ${investimentoMensal[03]}`);
-console.log(`O valor investido no mês de maio é: ${investimentoMensal[04]}`);
+console.log(`O valor investido no mês de março é: ${investimentoMensal[2]}`);
+console.log(`O valor investido no mês de abril é: ${investimentoMensal[3]}`);
+console.log(`O valor investido no mês de maio é: ${investimentoMensal[4]}`);
 
 //console.log(investimentoMensal);
---
+++ Aulas/Módulo_2/tiposreferencia.js
@@ -0,0 +1,23 @@
+let musica = {
+  nome: "Let it be",
+  artista: "The Beatles",
+  album: "Let it be",
+};
+
+let listaDeBandas = [
+  "Beatles",
+  "Rolling Stones",
+  "Led Zeppelin",
+  "Cassia Eller",
+  "Chico Buarque",
+];
+
+//para inserir um novo elemento no array. Inserir Skank em lista de bandas
+//console.log(listaDeBandas);
+//listaDeBandas[5] = "Skank";
+//console.log(listaDeBandas);
+
+//para inserir um novo elemento no objeto. Inserir o ano do álbum.
+console.log(musica);
+musica.ano = 1969;
+console.log(musica);
