a8f15917d3e85a66ae215db4329bcd6e61a4f4ab
Bruno Leão <bruno.melosh@gmail.com>
2024-08-04 20:57:07 -0400

    Aula sobre funções

---
+++ Aulas/Módulo_2/funcoes.js
@@ -0,0 +1,18 @@
+// function saudacao(nome) {
+//   console.log(`Olá ${nome}, bem vindo(a) ao nosso site!`);
+// }
+
+// saudacao("Karoline");
+
+// // Função para iniciar o cadastro
+// function enviarMensagem() {
+//   console.log("Para continuar você precisa informa o seu nome para cadastro.");
+// }
+// enviarMensagem();
+
+//Chamada de funções
+function exibirDetalhes(nome, curso) {
+  console.log("Professor: " + nome + "Curso: " + curso);
+}
+
+exibirDetalhes("Bruno", "JavaScript");
