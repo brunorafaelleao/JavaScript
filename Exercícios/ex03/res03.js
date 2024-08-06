6690273f8be281f4555106deb3bf46030cf0dc7d
Bruno Leão <bruno.melosh@gmail.com>
2024-08-06 10:00:10 -0400

    resolucao res03 questão 3

--- Exercícios/ex03/res03.js
+++ Exercícios/ex03/res03.js
@@ -1,18 +1,29 @@
-console.log("----Resolução da exercício 01----");
-function media(n1, n2, n3) {
-  const calculo = (n1 + n2 + n3) / 3;
-  console.log("A média desse aluno é: " + calculo);
-}
-media(3, 5, 1);
+// console.log("----Resolução da exercício 01----");
+// function media(n1, n2, n3) {
+//   const calculo = (n1 + n2 + n3) / 3;
+//   console.log("A média desse aluno é: " + calculo);
+// }
+// media(3, 5, 1);
 
-// console.log("\n\n----Resolução da exercício 02----\n");
-function mediafinal(n1, n2, n3, n4) {
-  const media1 = (n1 + n2 + n3) / 3;
-  const media2 = (media1 + n4) / 2;
-  console.log("A média final desse aluno é: " + media2);
-}
-mediafinal(3, 5, 1, 7);
+// console.log("\n----Resolução da exercício 02----");
+// function mediafinal(n1, n2, n3, n4) {
+//   const media1 = (n1 + n2 + n3) / 3;
+//   const media2 = (media1 + n4) / 2;
+//   console.log("A média final desse aluno é: " + media2);
+// }
+// //mediafinal(3, 5, 1, 7);
+// let n1 = 3;
+// n2 = 5;
+// n3 = 1;
+// n4 = 7;
+// mediafinal(n1, n2, n3, n4);
 
-//console.log("\n\n----Resolução da exercício 03----\n");
+console.log("\n----Resolução da exercício 03----");
+function tempFarenheit(valor) {
+  const tempF = 1.8 * valor + 32;
+  console.log(`A temperatura correspondente em Farenheit é de ${tempF}°F`);
+}
+let tempCelsius = 30;
+tempFarenheit(tempCelsius);
 
 //console.log("\n\n----Resolução da exercício 04----\n");
