5f1c2ad4618fa7f7b949e4cb1dbee975a410444c
Bruno Leão <bruno.melosh@gmail.com>
2024-08-06 00:51:31 -0400

    questao 1 e 2 resolivida ex03

--- Exercícios/ex03/res03.js
+++ Exercícios/ex03/res03.js
@@ -1,4 +1,18 @@
-console.log("----Resolução da questão 01----\n");
-console.log("\n\n----Resolução da questão 02----\n");
-console.log("\n\n----Resolução da questão 03----\n");
-console.log("\n\n----Resolução da questão 04----\n");
+console.log("----Resolução da exercício 01----");
+function media(n1, n2, n3) {
+  const calculo = (n1 + n2 + n3) / 3;
+  console.log("A média desse aluno é: " + calculo);
+}
+media(3, 5, 1);
+
+// console.log("\n\n----Resolução da exercício 02----\n");
+function mediafinal(n1, n2, n3, n4) {
+  const media1 = (n1 + n2 + n3) / 3;
+  const media2 = (media1 + n4) / 2;
+  console.log("A média final desse aluno é: " + media2);
+}
+mediafinal(3, 5, 1, 7);
+
+//console.log("\n\n----Resolução da exercício 03----\n");
+
+//console.log("\n\n----Resolução da exercício 04----\n");
