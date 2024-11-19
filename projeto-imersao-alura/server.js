2fff179191b0352aa60ef8103a5dc848303ddb05
Bruno Leão <bruno.melosh@gmail.com>
2024-11-19 15:02:51 -0400

    criando objetos para testar retorno da api

--- projeto-imersao-alura/server.js
+++ projeto-imersao-alura/server.js
@@ -1,11 +1,49 @@
 import express from "express";
 
+const posts = [
+  {
+    id: 1,
+    descricao: "primeira foto",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 2,
+    descricao: "segunda foto",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 3,
+    descricao: "terceira foto",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+  {
+    id: 4,
+    descricao: "quarta foto",
+    imagem: "https://placecats.com/millie/300/150",
+  },
+];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Servidor escutando");
 });
 
-//rotas
-app.get("/api",(req, res) =>{
-  res.status(200).send("Boas vindas ao servidor da aula sobre servidor em Node.js");
-})
+//rota para testar se o servidor está funcionando e retornar uma mensagem na tela
+function buscarPostPorID(id) {
+  return posts.findIndex((post) => {
+    return post.id === Number(id);
+  });
+}
+
+app.get("/posts/:id", (req, res) => {
+  const index = buscarPostPorID(req.params.id);
+  res.status(200).json(posts[index]);
+});
+
+app.get("/api", (req, res) => {
+  res
+    .status(200)
+    .send("Boas vindas ao servidor da aula sobre servidor em Node.js");
+});
